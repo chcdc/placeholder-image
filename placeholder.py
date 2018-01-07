76ee4b0a26368a90bf3579ecb7d7cf3b5777360b
carlos <carlos@04k.lan>
2018-01-07 00:41:39 -0200

    Insert Image Manipulation

--- placeholder.py
+++ placeholder.py
@@ -43,14 +43,26 @@ from django.http import HttpResponse, HttpResponseBadRequest
 class ImageForm(forms.Form):
 	height = forms.IntegerField(min_value=1, max_value=2000)
 	width = forms.IntegerField(min_value=1, max_value=2000)
-
+	
 	def generate(self,image_format='PNG'):
+		# Gera uma imagem do tipo especificado e retorna em forma de bytes puros
 		height = self.cleaned_data['height']
 		width = self.cleaned_data['width']
-		image = Image.new('RGB', (width, height))
-		content = BytesIO()
-		image.save(content, image_format)
-		content.seek(0)
+		key = (f"{width}.{height}.{image_format}" )
+		content = cache.get(key)
+		if content is None:
+			image = Image.new('RGB', (width, height))
+			draw = ImageDraw.Draw(image)
+			text = (f"{width} x {height}")
+			textwidth, textheight = draw.textsize(text)
+			if textwidth < width and textheight < height:
+				texttop = (height - textheight) // 2
+				textleft = (width - textwidth) // 2 
+				draw.text((textleft, texttop), text, fill=(255, 255, 255))
+			content = BytesIO()
+			image.save(content, image_format)
+			content.seek(0)
+			cache.set(key, content, 60*60)
 		return content
 
 
