0cdb32afbd993419fb1ca28ca50d8a9a827ba595
Carlos Carvalho <chenriquecdc@gmail.com>
2020-01-29 17:20:12 -0300

    Update modules

--- placeholder.py
+++ placeholder.py
@@ -44,7 +44,7 @@ settings.configure(
 from django import forms
 from django.conf.urls import url
 from django.core.cache import cache
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import etag
